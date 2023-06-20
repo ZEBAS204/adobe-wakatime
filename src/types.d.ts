87b5aea0ca27c95cef36cc108b8121add31282d1
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-06-20 19:41:27 -0300

    Fix node `path` types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,4 @@
+import type * as PATH from 'path'
 import type * as lodash from 'lodash'
 
 // Basic declarations as `any` until proper UXP types exist:
@@ -6,8 +7,9 @@ declare module 'uxp'
 // https://developer.adobe.com/photoshop/uxp/2022/uxp-api/reference-js/Modules/os/OS/
 declare module 'os'
 
-// This makes `_` available globally as a TS type:
-// lodash is available in the window scope at runtime
+// This makes types available globally.
+// Photoshop set those at runtime
 declare global {
 	const _: typeof lodash
+	const path: typeof PATH
 }
