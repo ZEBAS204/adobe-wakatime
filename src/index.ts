793251f4f8da8b197fd8167b19d40218bc682cca
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-06-21 20:14:14 -0300

    Add plugin index

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { app } from 'photoshop'
+import { saveSecureKey } from './storage'
+import { sendHeartbeat } from './wakatime'
+import { updateConnectionStatus } from './utils'
+import { ELEMENTS, CONFIG } from './constants'
+
+let intervalRef: NodeJS.Timer = null
+
+document
+	.getElementById(ELEMENTS.API_KEY_SAVE_BTN)
+	?.addEventListener('click', async () => {
+		console.log('[WakaTime] Updating API key')
+		await saveSecureKey()
+		init()
+	})
+
+const init = async () => {
+	clearInterval(intervalRef)
+	intervalRef = setInterval(async () => {
+		const heartbeatResponse = await sendHeartbeat({
+			file: app.activeDocument.name,
+			time: Date.now(),
+		})
+		console.log('heartbeat response:', heartbeatResponse)
+		updateConnectionStatus(heartbeatResponse)
+	}, CONFIG.HEARTBEAT_INTERVAL)
+}
+
+;(async () => {
+	console.log('[WakaTime] Initializing WakaTime plugin')
+	console.log('[WakaTime] Heartbeat interval:', CONFIG.HEARTBEAT_INTERVAL)
+	init()
+})()
