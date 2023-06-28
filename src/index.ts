0de68afbea1f9e25a43f8e68e84bd3709ac14da5
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-06-28 18:26:43 -0300

    Create WakaTimePlugin class

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,85 @@
-import { app } from 'photoshop'
-import {
-	saveSecureKey,
-	saveIsEnabled,
-	isExtensionEnabled,
-	manageStorage,
-	getMachineName,
-} from './storage'
-import { sendHeartbeat } from './wakatime'
+import './styles.css'
+import { ELEMENTS, CONFIG, STATUS } from './constants'
 import { updateConnectionStatus } from './utils'
-import { ELEMENTS, CONFIG } from './constants'
+import { sendHeartbeat } from './wakatime'
+import Storage from './storage'
 
-let intervalRef: NodeJS.Timer = null
+export class WakaTimePlugin {
+	private static intervalRef: NodeJS.Timer | null = null
 
-document
-	.getElementById(ELEMENTS.API_KEY_SAVE_BTN)
-	?.addEventListener('click', async () => {
+	public static async getActiveFile(): Promise<any> {}
+
+	public static initialize(): void {
+		console.log('[WakaTime] Initializing WakaTime plugin')
+		console.log(`[WakaTime] Heartbeat interval: ${CONFIG.HEARTBEAT_INTERVAL}ms`)
+
+		// Trigger update machine input value
+		Storage.getMachineName()
+
+		if (Storage.isExtensionEnabled()) this.init()
+	}
+
+	public static stop(): void {
+		clearInterval(this.intervalRef)
+	}
+
+	public static init(): void {
+		if (!Storage.isExtensionEnabled()) {
+			updateConnectionStatus(STATUS.DISCONNECTED)
+			return
+		}
+
+		this.intervalRef = setInterval(async () => {
+			const activeFile = await this.getActiveFile()
+			console.log('Active file:', activeFile)
+			if (!activeFile) return
+
+			const heartbeatResponse = await sendHeartbeat({
+				file: activeFile,
+				time: Date.now(),
+			})
+
+			console.log('heartbeat response:', heartbeatResponse)
+			updateConnectionStatus(heartbeatResponse)
+		}, CONFIG.HEARTBEAT_INTERVAL)
+	}
+
+	public static initListeners(): void {
+		document
+			.getElementById(ELEMENTS.API_KEY_SAVE_BTN)
+			?.addEventListener('click', this.handleApiKeySaveClick)
+
+		document
+			.getElementById(ELEMENTS.EXTENSION_ENABLED_CHECKBOX)
+			?.addEventListener('click', this.handleExtensionEnabledClick)
+
+		document
+			.getElementById(ELEMENTS.CONFIG_SAVE_BTN)
+			?.addEventListener('click', this.handleConfigSaveClick)
+
+		document
+			.getElementById(ELEMENTS.CONFIG_RESET_BTN)
+			?.addEventListener('click', this.handleConfigResetClick)
+	}
+
+	private static handleApiKeySaveClick = async (): Promise<void> => {
 		console.log('[WakaTime] Updating API key')
-		await saveSecureKey()
-		init()
-	})
+		await Storage.saveSecureKey()
+		this.init()
+	}
 
-document
-	.getElementById(ELEMENTS.EXTENSION_ENABLED_CHECKBOX)
-	?.addEventListener('click', () => {
-		saveIsEnabled()
-		init()
-	})
+	private static handleExtensionEnabledClick = (): void => {
+		Storage.saveIsEnabled()
+		this.init()
+	}
 
-document
-	.getElementById(ELEMENTS.CONFIG_SAVE_BTN)
-	?.addEventListener('click', () => {
-		manageStorage()
-		init()
-	})
+	private static handleConfigSaveClick = (): void => {
+		Storage.manageStorage()
+		this.init()
+	}
 
-document
-	.getElementById(ELEMENTS.CONFIG_RESET_BTN)
-	?.addEventListener('click', () => {
-		manageStorage(true)
-		init()
-	})
-
-const init = async () => {
-	clearInterval(intervalRef)
-	if (!isExtensionEnabled()) return
-
-	intervalRef = setInterval(async () => {
-		const activeFile = app.activeDocument.path || app.activeDocument.name
-		if (!activeFile) return
-		const heartbeatResponse = await sendHeartbeat({
-			file: activeFile,
-			time: Date.now(),
-		})
-		console.log('heartbeat response:', heartbeatResponse)
-		updateConnectionStatus(heartbeatResponse)
-	}, CONFIG.HEARTBEAT_INTERVAL)
+	private static handleConfigResetClick = (): void => {
+		Storage.manageStorage(true)
+		this.init()
+	}
 }
-
-;(async () => {
-	console.log('[WakaTime] Initializing WakaTime plugin')
-	console.log(`[WakaTime] Heartbeat interval: ${CONFIG.HEARTBEAT_INTERVAL}ms`)
-
-	// Trigger update machine input value
-	getMachineName()
-	if (isExtensionEnabled()) init()
-})()
