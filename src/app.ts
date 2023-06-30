96ade775ddd1dfb1efa64849b8eb40b7364f9c55
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-06-30 18:55:37 -0300

    Do not check machine name on init

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,6 @@ export class WakaTimePlugin {
 		console.log('[WakaTime] Initializing WakaTime plugin')
 		console.log(`[WakaTime] Heartbeat interval: ${CONFIG.HEARTBEAT_INTERVAL}ms`)
 
-		// Trigger update machine input value
-		Storage.getMachineName()
-
 		if (Storage.isExtensionEnabled()) this.init()
 	}
 
