b8deef359c415830e4797059cb492f085a099b0d
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-07-02 16:35:11 -0300

    Add missing SWC field label to UXP

--- src/index_uxp.ts
+++ src/index_uxp.ts
@@ -3,6 +3,8 @@ import '@spectrum-web-components/theme/sp-theme.js'
 import '@spectrum-web-components/theme/src/themes.js'
 // SWC Components
 import '@spectrum-web-components/status-light/sp-status-light.js'
+import '@spectrum-web-components/field-label/sp-field-label.js'
+
 // Local
 import { app } from 'photoshop'
 import { WakaTimePlugin } from './app'
